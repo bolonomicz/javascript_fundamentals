66556724408d59cde07a8ba56d2151031d20b7a8
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-23 21:22:06 -0400

    finish variables

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,6 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
-
+var varname = ["colors", "presidents", "whatever"];
+console.log("My 1st choice " + varname[0], "My 2nd choice " + varname[1], "My 3rd choice " + varname[2]);
 
--- js_fundamentals/variables.js
+++ js_fundamentals/variables.js
@@ -6,6 +6,12 @@
 
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+var children = 4;
+var name = "Joe-An";
+var city = "Toronto";
+var job = "Junior Developer";
+console.log("You will be a " + job + " in " + city + ", and married to " + name + " with " + children + " kids.");
+
 
 // EXERCISE: The Age Calculator
 
@@ -15,6 +21,9 @@
 // Store their birth year in a variable.
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
+var currentYear = 2013;
+var birthYear = 1993;
+console.log("They are " + (currentYear - birthYear))
 
 // EXERCISE: The Lifetime Supply Calculator
 
@@ -25,6 +34,12 @@
 // Store an estimated amount per day (as a number).
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
+var currentAge = 20;
+var maxAge = 100;
+var snackADay = 4;
+var lifeSupply = (maxAge - currentAge) * 365 * snackADay;
+console.log("You will need " + lifeSupply + " to last you until the ripe old age of " + maxAge + ".");
+
 
 // EXERCISE: The Geometrizer
 
@@ -33,6 +48,12 @@
 // Store a radius into a variable.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
+
+var radius = 10;
+var circumference = radius * 2 * 3.13;
+var area = 3.14 * radius * radius;
+console.log("The circumference is " + circumference + ".");
+console.log("The area is " + area + ".")
 
 // EXERCISE: The Temperature Converter
 
@@ -43,4 +64,11 @@
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
 
+var celsius = 20;
+var celsiusToF = (celsius * (9/5)) + 32;
+console.log(celsius + "C is " + celsiusToF + " f")
+
+var fahrenheit = 100;
+var fToC = (fahrenheit - 32) * (5/9);
+console.log(fahrenheit + "f is " + fToC + " c")
 
