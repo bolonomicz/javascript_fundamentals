e789ae91ae70180fb5e9832b853c2b92d3619aa0
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-24 14:33:38 -0400

    finish objects

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -32,16 +32,27 @@ console.log(recipe.ingredients[2]);
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 var books = [
-	{title: "Holes",
-	 author: "SomeGuy"
-	 alreadyRead: false},
-	 {title: "Another Book Title",
-	 author: "Anotherguy"
-	 alreadyRead: true},
-	 {title: "third book title",
-	 author: "the third guy",
-	 alreadyRead: false}
-]
+  {title: "Holes", 
+   author: "Bob",
+ 	 alreadyRead: true},
+  {title: "holes3", 
+   author: "Bob",
+ 	 alreadyRead: false},
+ 	 {title: "That book", 
+   author: "That author",
+ 	 alreadyRead: true},
+];
+for (var i = 0; i < books.length; i++ ){
+	var book = books[i]
+	console.log(book.title + " by " + book.author);
+	var bookStatement = book.title + " by " + book.author
+	if ( book.alreadyRead === true){
+		console.log("You already read " + bookStatement);
+	}
+	else{
+		console.log("You need to read " + bookStatement);
+	}
+}
 
 // EXERCISE: The Movie Database
 
@@ -51,4 +62,20 @@ var books = [
 // Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
 
 
+var movies = {
+	title: "Holes",
+	duration: 120,
+	stars: [ "pasta", "meat", "tomato" ]
+};
+
+
+function describeMovie(movie) {
+	for (var i = 0; i < movies.stars.length; i++) {
+   
+}
+  console.log(movie.title + " lasts for " + movie.duration + "minutes. Stars:" + movie.stars); 
+}
+
+
+describeMovie(movies) 
 
