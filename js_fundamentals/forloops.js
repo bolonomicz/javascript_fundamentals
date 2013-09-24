eb42246da8d2e5653882766e58198b791a8485aa
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-24 13:47:11 -0400

    finish forloops

--- js_fundamentals/forloops.js
+++ js_fundamentals/forloops.js
@@ -3,15 +3,48 @@
 // EXERCISE: The even/odd reporter
 
 // Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+var count = 0;
+while (count < 21) {
+	if ( count % 2 === 0){
+		console.log(count + " is even");
+	}
+	else{
+		console.log(count + " is odd.")
+	}
+		count++;
+}
 
 // EXERCISE: Multiplication Tables
 
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+for(count = 0; count < 11; count++){
+	var num = count * 9
+	console.log(count + "* 9 = "+ num );
+}
 
 // Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
 
 // EXERCISE: The Grade Assigner
 
 // Check the results of assignGrade function from the conditionals exercise for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
+function assignGrade(score){
+	if ( score <= 50){
+		console.log("F");
+	}
+	else if ( score > 50 && score <= 60){
+		console.log("D");
+	}
+	else if ( score > 60 && score <=70){
+		console.log("C");
+	}
+	else if ( score > 70 && score <= 80)
+		console.log("B");
+	else {
+		console.log("A");
+	}
+}
 
+for(count = 60; count < 101; count++){
+	console.log(assignGrade(count));
+}
 
--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -13,6 +13,17 @@
 // cumin
 // cocoa
 
+var recipe = {
+	title: "lasagna",
+	servings: 4,
+	ingredients: [ "pasta", "meat", "tomato" ]
+};
+
+console.log(recipe.title);
+console.log("Serves: " + recipe.servings);
+console.log(recipe.ingredients[0]);
+console.log(recipe.ingredients[1]);
+console.log(recipe.ingredients[2]);
 // EXERCISE: The Reading List
 
 // Keep track of which books you read and which books you want to read!
@@ -20,6 +31,17 @@
 // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+var books = [
+	{title: "Holes",
+	 author: "SomeGuy"
+	 alreadyRead: false},
+	 {title: "Another Book Title",
+	 author: "Anotherguy"
+	 alreadyRead: true},
+	 {title: "third book title",
+	 author: "the third guy",
+	 alreadyRead: false}
+]
 
 // EXERCISE: The Movie Database
 
